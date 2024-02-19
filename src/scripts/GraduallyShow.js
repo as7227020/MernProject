429c282922d7edb28616229cad78af291686865d
Zin_User <as7227020@gmail.com>
2024-02-19 18:21:06 +0800

    顯示工具優化 - 增加一些參數可設定(CSS套用JS參數)

--- src/scripts/GraduallyShow.js
+++ src/scripts/GraduallyShow.js
@@ -1,12 +1,33 @@
 import React,{useEffect , useRef} from 'react'
 import './GraduallyShow.css'
+
+function GetCustomStyle(Props) {
+  return  {
+    '--my-delay-style': `${Props.delay}s`,
+    '--my-playSpeed-style': `${Props.playSpeed}s`,
+    '--my-showDistanceX-style': `${Props.distanceX}px`,
+    '--my-showDistanceY-style': `${Props.distanceY}px`,
+  };
+}
+
+
 /*
 逐漸顯示
 obj = 物件
-isReset = 是否會重製(再回來時就在顯示一次)
-*/
+isReset = 是否會重置(再回來時就在顯示一次)
+delay = 延遲幾秒顯示
+distance = 到顯示目標的位置得移動距離長度
 
-export function GraduallyShow({obj,isReset = false}){
+用法: import GraduallyShow 後
+  <GraduallyShow  delay={3.5} playSpeed={1.5} distanceX={0} distanceY={0} obj={
+          物件
+           }/>
+
+
+*/
+export function GraduallyShow({obj, isReset=false, delay=0, playSpeed=1, distanceX=0, distanceY=0}){
+
+
     const ref = useRef(null);
   
     useEffect(() => {
@@ -33,7 +54,7 @@ export function GraduallyShow({obj,isReset = false}){
   
       return () => observer.disconnect();
     }, []);
-  
-    return <div ref={ref} >{obj}</div>;
+    
+    return <div ref={ref} style={GetCustomStyle({delay,playSpeed,distanceX,distanceY})}>{obj}</div>;
   };
   
